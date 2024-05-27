4e01737686d416029ca4ff0bbc367674163b61d3
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-05-27 19:57:44 +0200

    Updated ft_printf.c
    Added macros usage to formatter function
    Shortened ft_strlen function
    Deleted unnecessary checks

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ana-cast <ana-cast@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 19:17:07 by ana-cast          #+#    #+#             */
-/*   Updated: 2023/09/23 17:57:52 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/05/27 19:57:21 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	ft_strlen(const char *str)
 	int	count;
 
 	count = 0;
-	while (str[count])
-		count++;
+	while (str[++count])
+		;
 	return (count);
 }
 
@@ -31,15 +31,15 @@ static void	select_format(va_list arg, int value, int *result)
 	else if (value == 'p')
 		ft_pf_pointer(va_arg(arg, unsigned long long), result);
 	else if (value == 'd' || value == 'i')
-		ft_pf_putnbr_base(va_arg(arg, int), "0123456789", result);
+		ft_pf_putnbr_base(va_arg(arg, int), DECIMAL, result);
 	else if (value == 'u')
-		ft_pf_putnbr_base(va_arg(arg, unsigned int), "0123456789", result);
+		ft_pf_putnbr_base(va_arg(arg, unsigned int), DECIMAL, result);
 	else if (value == 'x')
-		ft_pf_putnbr_base(va_arg(arg, unsigned int), "0123456789abcdef",
-			result);
+		ft_pf_putnbr_base(va_arg(arg, unsigned int), HEXA_LOW, result);
 	else if (value == 'X')
-		ft_pf_putnbr_base(va_arg(arg, unsigned int), "0123456789ABCDEF",
-			result);
+		ft_pf_putnbr_base(va_arg(arg, unsigned int), HEXA_CAP, result);
+	else if (value == '%')
+		ft_pf_putchar('%', result);
 	else
 		*result = -1;
 }
@@ -53,18 +53,12 @@ int	ft_printf(char const *str, ...)
 	i = 0;
 	result = 0;
 	va_start(arg, str);
-	while (str[i] && str && result != -1)
+	while (str[i])
 	{
 		if (str[i] != '%')
 			ft_pf_putchar(str[i], &result);
 		else if (str[i + 1])
-		{
-			i++;
-			if (str[i] == '%')
-				ft_pf_putchar('%', &result);
-			else
-				select_format(arg, str[i], &result);
-		}
+			select_format(arg, str[++i], &result);
 		i++;
 	}
 	va_end(arg);
